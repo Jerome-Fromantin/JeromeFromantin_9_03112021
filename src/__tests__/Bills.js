4a1ca89ca3ffc53348fcc4128d41d848179d0894
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2021-11-21 19:02:47 +0100

    Début de mise en place d'un test pour le layout vertical

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,13 +1,34 @@
 import { screen } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
+import VerticalLayout from '../views/VerticalLayout.js' // Ajout de code.
 import { bills } from "../fixtures/bills.js"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: []})
-      document.body.innerHTML = html
       //to-do write expect expression
+      // Vérifier que la <div id='layout-icon1'> a aussi la classe "active-icon".
+      const html = VerticalLayout()
+      let user;
+      user = JSON.parse(localStorage.getItem('user'))
+      console.log(typeof user)
+      if (typeof user === 'string') {
+        user = JSON.parse(user)
+        console.log("Oui !")
+      } else {
+        console.log("Mince !")
+      }
+      if (user && user.type === 'Employee') {
+        console.log("Tu y es presque !")
+        console.log(user)
+        console.log(user.type)
+        document.body.innerHTML = html
+        console.log(html)
+      } else {
+        console.log("Fuck !")
+      }
+      //const windowIcon = screen.getByTestId("icon-window")
+      //expect(windowIcon).toBeVisible()
     })
     test("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills })
