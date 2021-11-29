636ef55aa61dd18a9f0bda649eca9dead1b130ea
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2021-11-29 18:15:22 +0100

    Légères corrections de texte

--- src/__tests__/ErrorPage.js
+++ src/__tests__/ErrorPage.js
@@ -2,7 +2,7 @@ import { screen } from "@testing-library/dom"
 import ErrorPage from "../views/ErrorPage.js"
 
 describe('Given I am connected on app (as an Employee or an HR admin)', () => {
-  describe('When ErrorPage is called without and error in its signature', () => {
+  describe('When ErrorPage is called without an error message in its signature', () => {
     test(('Then, it should render ErrorPage with no error message'), () => {
       const html = ErrorPage()
       document.body.innerHTML = html
@@ -10,7 +10,7 @@ describe('Given I am connected on app (as an Employee or an HR admin)', () => {
       expect(screen.getByTestId('error-message').innerHTML.trim().length).toBe(0)
     })
   })
-  describe('When ErrorPage is called with error message in its signature', () => {
+  describe('When ErrorPage is called with an error message in its signature', () => {
     test(('Then, it should render ErrorPage with its error message'), () => {
       const error = 'Erreur de connexion internet'
       const html = ErrorPage(error)
