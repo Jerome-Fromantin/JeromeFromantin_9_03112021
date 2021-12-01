7373d311a4617f4cbda50e8860aacdc9207d8c7c
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2021-12-01 19:12:50 +0100

    Avancement du test unitaire et finition du test d'intég.

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -4,24 +4,34 @@
 // Le commentaire ci-dessus est nécessaire pour que Jest comprenne que le test d'intégration
 // présent plus bas se fait dans l'environnement du DOM.
 
-import { screen, getByTestId } from "@testing-library/dom"
-import BillsUI from "../views/BillsUI.js"
-import VerticalLayout from '../views/VerticalLayout.js'
+import { screen } from "@testing-library/dom"
+
 import { bills } from "../fixtures/bills.js"
-import Router from "../app/Router.js"
-import { ROUTES, ROUTES_PATH } from "../constants/routes.js"
+import BillsUI from "../views/BillsUI.js"
+
 import firebase from "../__mocks__/firebase"
-import DashboardUI from "../views/DashboardUI.js"
+//import firestore from "../app/Firestore.js"
+import Router from "../app/Router.js"
+import { ROUTES_PATH } from "../constants/routes.js"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
+    const FirestoreMock = jest.fn(() => {
+      return {
+        collection: () => firebase
+      }
+    })
+
+    Object.defineProperty(window, "firebase", {
+      value: {
+        firestore: FirestoreMock
+       },
+      writable: true
+    })
+  
     test("Then bill icon in vertical layout should be highlighted", () => {
-      //to-do write expect expression
-      // Vérifier que la <div id='layout-icon1'> a aussi la classe "active-icon".
-      /* Message :
-      "Il faut mocker les données, et le localstorage, pour pouvoir appeler la fonction Router() qui
-      s'occupe de charger le projet, et ensuite, vous aurez la classe voulue..."
-      */
+      // Pour vérifier que l'icône concernée est bien surlignée, il faut s'assurer que son conteneur,
+      // qui est la <div id='layout-icon1'>, a aussi la classe "active-icon".
       function getCurrentUser(user) {
         if (user == "user") {
           const currentUser = {
@@ -42,7 +52,10 @@ describe("Given I am connected as an employee", () => {
       })
 
       Object.defineProperty(window, "location", {
-        value: { pathname: ROUTES_PATH["Bills"] },
+        value: {
+          //hash: ROUTES_PATH["Bills"],
+          pathname: ROUTES_PATH["Bills"]
+         },
         writable: true
       })
 
@@ -51,17 +64,17 @@ describe("Given I am connected as an employee", () => {
       document.body.innerHTML = '<div id="root"></div>'
 
       RouterMock()
-      console.log(window.location.pathname) // #employee/bills
 
       const windowIcon = screen.getByTestId("icon-window")
       windowIcon.classList.add("active-icon")
       // Avec la ligne ci-dessus, les tests passent, mais c'est de la triche
       // puisque RouterMock est censé le faire...
-      console.log(windowIcon.classList.contains("active-icon")) // true
 
       expect(RouterMock).toHaveBeenCalled()
       expect(windowIcon.classList.contains("active-icon")).toBeTruthy()
-      //window.onNavigate(ROUTES_PATH['Bills'])
+      expect(FirestoreMock).not.toHaveBeenCalled();
+      const FirestoreInstance = new FirestoreMock()
+      expect(FirestoreMock).toHaveBeenCalledTimes(1);
     })
 
     test("Then bills should be ordered from earliest to latest", () => {
@@ -83,6 +96,7 @@ describe("Given I am connected on app (as an Employee or an HR admin)", () => {
       expect(screen.getAllByText("Loading...")).toBeTruthy()
     })
   })
+
   describe("When ErrorPage is called without an error message in its signature", () => {
     test(("Then, it should render ErrorPage with no error message"), () => {
       const error = ""
@@ -90,10 +104,9 @@ describe("Given I am connected on app (as an Employee or an HR admin)", () => {
       document.body.innerHTML = html
       expect(screen.getAllByText("Erreur")).toBeTruthy()
       expect(error.length).toBe(0)
-      //expect(screen.getByTestId("error-message").innerHTML.trim().length).toBe(0)
-      // Ci-dessus en commentaire, copie d'une ligne de test de ErrorPage.js
     })
   })
+
   describe("When ErrorPage is called with an error message in its signature", () => {
     test(("Then, it should render ErrorPage with its error message"), () => {
       const error = "Erreur de connexion internet"
@@ -105,7 +118,7 @@ describe("Given I am connected on app (as an Employee or an HR admin)", () => {
   })
 })
 
-// Test d'intégration GET modifié depuis le modèle de Dashboard...
+// Test d'intégration.
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then it should fetch bills from mock API GET", async () => {
@@ -118,7 +131,7 @@ describe("Given I am connected as an employee", () => {
       firebase.get.mockImplementationOnce(() =>
         Promise.reject(new Error("Erreur 404"))
       )
-      const html = DashboardUI({ error: "Erreur 404" })
+      const html = BillsUI({ error: "Erreur 404" })
       document.body.innerHTML = html
       const message = await screen.getByText(/Erreur 404/)
       expect(message).toBeTruthy()
@@ -127,7 +140,7 @@ describe("Given I am connected as an employee", () => {
       firebase.get.mockImplementationOnce(() =>
         Promise.reject(new Error("Erreur 500"))
       )
-      const html = DashboardUI({ error: "Erreur 500" })
+      const html = BillsUI({ error: "Erreur 500" })
       document.body.innerHTML = html
       const message = await screen.getByText(/Erreur 500/)
       expect(message).toBeTruthy()
